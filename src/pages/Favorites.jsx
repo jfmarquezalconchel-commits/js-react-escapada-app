80ca009a396eb20bbd5ff671d6e9c4d248ace76b
jfmarquezalconchel-commits <jf.marquez.alconchel@gmail.com>
2026-05-11 10:55:14 +0200

    continuamos

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,25 @@
+import { plans } from "../data/plans.js";
+import Tarjeta from "../components/Tarjeta.jsx";
+
 function Favorites() {
+
+ let favorites=localStorage.getItem("favorites");
+ favorites = (favorites==null ? [] : JSON.parse(favorites));
+ console.log(favorites);
+ 
+
   return (
     <div>
       <h1>Favorites</h1>
       <p>This is the favorites page where you can see your favorite planes.</p>
+      {favorites.length === 0 ? (
+        <p>No favorites yet.</p>
+      ) : (
+        favorites.map((favorite, index) => (
+          <Tarjeta plan={plans.find(plani=>plani.id==favorite)} isFavorite={true}/>
+        ))
+      )}
+
     </div>
   )
 }   
