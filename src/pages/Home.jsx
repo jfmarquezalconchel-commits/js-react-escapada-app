601c113f2ebee6dc2f7f5d7866466d82830bcf34
jfmarquezalconchel-commits <jf.marquez.alconchel@gmail.com>
2026-05-11 12:35:14 +0200

    vacaciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import heroImg from '../assets/hero.png';
+import heroImg from '../assets/hollydays.jpg';
 import relaxImg1 from '../assets/relax1.jpg';
 import relaxImg2 from '../assets/relax2.jpg';
 import relaxImg3 from '../assets/relax3.jpg';
